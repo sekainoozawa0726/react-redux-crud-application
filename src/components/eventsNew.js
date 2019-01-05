9e10bfdee9fc922de9ce8eecb9fdcf5cece7976a
sekainoozawa0726 <150fe308@st.nufs.ac.jp>
2019-01-05 15:07:47 +0900

    install: redux-form

--- src/components/eventsNew.js
+++ src/components/eventsNew.js
@@ -1,17 +1,44 @@
 import React,{ Component } from 'react';
 
 import { connect } from 'react-redux';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import { postEvent } from '../actions';
+import { Field, reduxForm } from 'redux-form';
 
 class EventsNew extends Component {
+  renderField (field) {
+    const { input, label, type, meta: { touched, error } } = field;
+    return (<div></div>)
+  }
+
   render() {
     return(
-      <div>hoge</div>
+      <div>
+        <form>
+          <div>
+            <Field label='Title' name='title' type='text' component={this.renderField} />
+          </div>
+          <div>
+            <Field label='Body' name='body' type='text' component={this.renderField} />
+          </div>
+          <div>
+            <input type='submit' value='submit' disabled='false' />
+            <Link to='/'>cancel</Link>
+          </div>
+        </form>
+      </div>
     );
   }
 }
 
 // const mapDispatchToProps = ({ postEvent });
 
-export default connect(null, null)(EventsNew);
+const validate = values => {
+  const errors = {}
+
+  return errors
+}
+
+export default connect(null, null)(
+  reduxForm({ validate, form: 'eventsNewForm' })(EventsNew)
+  );
