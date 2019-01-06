3e3de961b23448f8169be175fea929f785dee659
sekainoozawa0726 <150fe308@st.nufs.ac.jp>
2019-01-06 18:58:00 +0900

    add: server file made of node.js

---
+++ server/server.js
@@ -0,0 +1,24 @@
+let express = require('express');
+
+let app = express();
+app.set('port', process.env.PORT || 5000);
+
+let clientPath = __dirname.replace('/server', '/client');
+app.use('/', express.static(clientPath));
+
+// app.use(function(req, res, next){
+//   res.status(404);
+//   res.sendFile(clientPath + '/index.html');
+// });
+
+
+//エラー発生時の処理
+app.use(function(err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).send(err.message);
+});
+
+//指定したポートでリクエスト待機状態にする
+app.listen(app.get('/port'), function () {
+  console.log('server listening on port :' + app.get('port'));
+});
--- src/actions/index.js
+++ src/actions/index.js
@@ -4,18 +4,11 @@ export const READ_EVENTS = 'READ_EVENTS';
 const CREATE_EVENT = 'CREATE_EVENT';
 
 // CRUD処理の際に使うベースURL
-const ROOT_URL = 'http://jws.jalan.net/APICommon/OnsenSearch/V1/';
-const QUERY_STRING = '?count:10';
-const config = {
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'api' : 'a2caf03d1b559759'
-  }
-}
-  
+const ROOT_URL = 'http://jws.jalan.net/APIAdvance/StockSearch/V1/';
+const QUERY_STRING = '?sauna:1';
 
 export const readEvents = () => async dispatch => {
-  const response = await axios.get(`${ROOT_URL}${QUERY_STRING}`,config);
+  const response = await axios.get(`${ROOT_URL}${QUERY_STRING}`);
   dispatch({ type: READ_EVENTS, response });
 }
 
